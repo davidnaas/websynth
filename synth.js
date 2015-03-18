44e604662b3160ceba0e67c01c455f050cc93a53
David Nääs <david_naas@hotmail.com>
2015-03-18 13:02:12 +0100

    cleaned up css

--- synth.js
+++ synth.js
@@ -11,13 +11,12 @@ var isEmpty = 	function(obj) {
 var stringToFreq = {'A': 261.36, 'W': 277.2, 'S': 293.7 , 'E': 311.1, 'D': 329.6, 'F': 349.2, 'T': 370.0, 'G': 392.0, 'Y': 415.3, 'H': 440,
 						'U': 466.2, 'J': 493.9, 'K': 523.3};
 
-window.addEventListener('load', main, false);
 window.addEventListener('keydown', makeNote, false);
 window.addEventListener('keyup', stopNote, false);
 
 
 
-function main () {
+$(document).ready(function  () {
 	try{
 		window.AudioContext = window.AudioContext||window.webkitAudioContext;
 		context = new AudioContext() 
@@ -44,15 +43,21 @@ function main () {
 	osc2.connect(gain1.getGain());
 	gain1.connect(context.destination)
 	osc1.start(0);
-}
+
+	
+});
+
+
 
 function makeNote (e) {
 	var freq = stringToFreq[String.fromCharCode(e.keyCode)];
+	console.log(freq);
 	console.log(String.fromCharCode(e.keyCode));
 	osc1.setFreq(freq);
 	osc2.setFreq(freq);
 
-	gain1.setGain(1);
+	if(freq != undefined)
+		gain1.setGain(1);
 	depressed_keys[e.keyCode] = true;
 }
 
