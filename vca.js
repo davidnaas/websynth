d5c0ccf622a52b0152888cad7c91b0847ebb3d58
David Nääs <david_naas@hotmail.com>
2015-05-19 14:38:11 +0200

    renaming to vca and such

---
+++ vca.js
@@ -0,0 +1,24 @@
+function VCA () {
+	var gain;
+
+	function init (externalContext) {
+		gain = externalContext.createGain();
+	}
+	function setGain (gainValue) {
+		gain.gain.value = gainValue;
+	}
+
+	function connect (connectTo) {
+		gain.connect(connectTo);
+	}
+	function getGain () {
+		return gain;
+	}
+
+	return Object.freeze({
+			init: init,
+			setGain: setGain,
+			getGain: getGain,
+			connect: connect
+	});
+}
